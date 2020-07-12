0852ec6f76702630c7416a21270eaf61c9360a93
liuyan0815 <1411343694@qq.com>
2020-07-12 19:05:37 +0800

    L1uEan的每时每刻项目作业

---
+++ web02/src/router/index.js
@@ -0,0 +1,101 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// 五个导航首位
+import Home from '../view/Home'
+import KeCheng from '../view/KeCheng';
+import LianXi from '../view/LianXi';
+import Me from '../view/User/Me';
+import YueKe from '../view/YueKe';
+import Index from '../view/TeacherXq/Index'
+
+
+  
+
+Vue.use(Router)
+
+
+let r = new Router({
+  routes: [
+    {
+      path:'/',
+      redirect:'/home/index'
+    },
+    {
+      path: '/home',
+      name:'Home',
+      component:Home,
+      children:[
+        {
+          path:'index',
+          name:'Index',
+          component:Index
+        },
+        {
+          path:'lianxi',
+          name:'LianXi',
+          component:LianXi
+        },
+        {
+          path:'kecheng',
+          name:'KeCheng',
+          component:KeCheng
+        },
+        {
+          path:'person',
+          component:()=> import ('../view/User/PerSon'),
+          // beforeEnter:((to,from,next)=>{
+          //   // if(localStorage.getItem('token')){
+            
+          //   // console.log(localStorage.getItem('token'))
+          //   //   next()
+          //   // }else{
+          //   //   next('/me')
+          //   // }
+          //   let token = localStorage.getItem('token')
+          //   if(!token && to.name.indexOf('Me') < 0){
+          //     next('/me')
+          //   } else {
+          //     next()
+          //   }
+           
+          // })
+          
+        },
+        {
+          path:'YueKe',
+          name:'YueKe',
+          component:YueKe
+        },
+      ]
+    },
+    //User. 注册界面
+    {
+      path:'/login',
+      name:'Login',
+      // 路由懒加载 当点击的时候才会调用
+      component:() => import('../view/User/Login')
+    },
+    // User.设置密码页面
+    {
+      path:'/passworld',
+      name:'PassWorld',
+      component:() => import('../view/User/PassWorld')
+    },
+    // User.登录密码页面
+    {
+      path:'/me',
+      name:'Me',
+      component:Me
+    },
+    // 讲师详情部分
+    {
+      path:'/teacher',
+      name:'TeacherMsg',
+      component:()=> import ('../view/TeacherXq/TeacherMsg')
+    },
+
+    
+  ]
+})
+
+export default r
---
+++ web02/src/view/User/PerSon.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="PerSon">
+      <div class="Person-top-box">
+          <!-- user用户img界面部分 -->
+          <div class="Person-user">
+                <h3>已经登录</h3>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+name:"PerSon",
+data() {
+return {
+
+}
+},
+//生命周期 - 创建完成（访问当前this实例）
+created() {
+
+},
+//生命周期 - 挂载完成（访问DOM元素）
+mounted() {
+
+}
+}
+</script>
+<style scoped>
+/* @import url(); 引入css类 */
+#PerSon{
+    overflow: hidden;
+    padding-bottom: 2rem;
+    padding-bottom: 20vw;
+}
+
+.Person-top-box{
+    height: 5.8rem;
+    height: 58vw;
+    background: url('../../assets/img/user_bg.png') no-repeat no-repeat;
+    background-size: 100% 1.3rem;
+    background-color: #fff;
+    position: relative;
+    padding: 0 .4rem;
+    padding: 0 4vw;
+}
+</style>
